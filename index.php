8f59f0aff341480ad757887faeff63b7bc8fb81e
Тырыкин Всеволод <sevok73@mail.ru>
2017-02-19 16:18:20 +0400

    Add files via upload
    Начальная страница и код к ней

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+<title> Сайт</title>
+<link rel="stylesheet" href="/style.css" type="text/css"/>
+</head>
+<body>
+
+<div id="welcome" class="post">
+	<div class="title">
+		<h2>Welcome to website by Tyrykin Vsevolod</h2>
+	</div>
+<div id="menu">
+	<ul>
+		<li class="menu"><a href="#" title="">Home</a></li>
+		<li><a name="#" title="">Gallery</a></li>
+		<li><a name="#" title="">About me</a></li>
+		<li><a name="#" title="">Contact</a></li>
+	</ul>
+</div>
+<div id="sidebar">
+		<div id="login" class="boxed">
+			<div class="title">
+				<h2>Client Account</h2>
+	<div class="Register">
+		<form id="form1" method="post" action="#">
+			<fieldset>					
+				<label for="email">Client E-mail:</label>
+				<input id="email" type="email" name="email" placeholder="enter your email" value="" />
+				<label for="text">Password:</label>
+				<input id="text" type="password" name="text" placeholder="enter your password" value="" />
+				<input id="submit1" type="submit" name="submit1" value="Sign In" />
+				<p><a href="#">Forgot your password?</a></p>
+			</fieldset>
+		</form>
+			</div>
+		</div>
+	</div>
+</div>	
+<div class="background">
+<style>
+   body {
+    background-image: url(/images/fon.jpg); 
+    background-attachment: fixed; 
+	color: #fff;
+   }
+  </style>
+
+<section>
+		<article>
+			<h2>Данный сайт создан для обучения студента Всеволода Тырыкина языку программирования PHP</h2>
+		</article>
+	</section>	
+
+</div>
+</body>
+</html>
